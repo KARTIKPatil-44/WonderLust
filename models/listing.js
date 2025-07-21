7dd3634bb6b81515ca46299c327ceec1f69f7e53
KARTIK-44 <kartikpatilcs@gmail.com>
2025-07-21 22:32:47 +0530

    solved bug of image link

--- models/listing.js
+++ models/listing.js
@@ -8,8 +8,9 @@ const ListingSchema = new Schema({
   },
   description: String,
   image: {
-    type: Object,
-    default :"tps://images.unsplash.com/photo-1584132967334-10e028bd69f7?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    type: String,
+    default:
+      "https://images.unsplash.com/photo-1584132967334-10e028bd69f7?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     set: (v) =>
       v === ""
         ? "https://images.unsplash.com/photo-1584132967334-10e028bd69f7?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
@@ -23,6 +24,3 @@ const ListingSchema = new Schema({
 
 const Listing = mongoose.model("Listing", ListingSchema);
 module.exports = Listing;
-  
-
-
